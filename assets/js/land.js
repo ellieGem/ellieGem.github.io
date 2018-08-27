831074280bd02c4d34258036a12f204a2369e579
ellieGem <38253879+ellieGem@users.noreply.github.com>
2018-08-27 16:21:44 +0000

    Update land.js

--- assets/js/land.js
+++ assets/js/land.js
@@ -1,21 +1,31 @@
-(function() {
-
-                var navLinks = $('nav ul li a'),
-                    navH = $('nav').height(),
-                    section = $('section'),
-                    documentEl = $(document);
-                
-                documentEl.on('scroll', function() {
-                    var currentScrollPos = documentEl.scrollTop();
-                    
-                    section.each(function() {
-                        var self = $(this);
-                        if ( self.offset().top < (currentScrollPos + navH) && (currentScrollPos + navH) < (self.offset().top + self.outerHeight()) ) {
-                            var targetClass = '.' + self.attr('class') + '-marker';
-                            navLinks.removeClass('active');
-                            $(targetClass).addClass('active');
-                        }
-                    });
-                    
-                });
-                })();
+$(function(){
+  var naviOffsetTop = $('navi').offset().top;
+  var homeOffsetTop = $('.home').offset().top;
+  var aboutOffsetTop = $('.about').offset().top;
+  var whyOffsetTop = $('.why').offset().top;
+  var howOffsetTop = $('.how').offset().top;
+  var servicesOffsetTop = $('.services').offset().top;
+  var contactOffsetTop = $('.contact').offset().top;
+  
+  $(document).on('scroll',function(){
+    var scrollTop = $(document).scrollTop;
+    var activeLi;
+    if(scrollTop < naviOffsetTop){
+      activeLi= $('.items >li:nth-child(1)');
+    }else if(scrollTop < homeOffsetTop){
+      activeLi= $('.items >li:nth-child(2)');
+    }else if( scrollTop < aboutOffsetTop){
+      activeLi= $('.items >li:nth-child(3)');
+       }else if(scrollTop < whyOffsetTop){
+         activeLi = $('.items >li:nth-child(4)');
+    }else if(scrollTop < howOffsetTop){
+      activeLi= $('.items >li:nth-child(5)');
+     }else if(scrollTop < servicesOffsetTop ){
+       activeLi= $('.items >li:nth-child(6)');
+     }else{
+     activeLi = $('.items >li:nth-child(7)');
+     }
+  activeLi.addClass('.active');
+  $('.items>li').not(activeLi).removeClass('active');
+  });
+});
